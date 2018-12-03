bf31e69b70d3beedc4bbc97c28d9453713082b11
Amber Wu <yuejun.wu1115@gmail.com>
2018-12-03 03:28:09 -0600

    edit on setting

--- ourproject/settings.py
+++ ourproject/settings.py
@@ -136,5 +136,10 @@ MEDIA_ROOT = '/home/securestroll/public_html/media'
 LOGIN_REDIRECT_URL = 'profile'
 LOGIN_URL = 'login'
 
-
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 
+# EMAIL_HOST_PASSWORD = 
 
