8c56a39e2ae8b2b182804817f42d5b9bf3273505
Amber Wu <yuejun.wu1115@gmail.com>
2018-12-02 22:02:00 -0600

    add update form

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 # from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
 	if request.method == 'POST':
@@ -19,7 +19,15 @@ def register(request):
 # Users can only see this page after they log in
 @login_required
 def profile(request):
-	return render(request, 'users/profile.html')
+	u_form = UserUpdateForm()
+	p_form = ProfileUpdateForm()
+
+	context = {
+		'u_form': u_form,
+		'p_form': p_form
+	}
+
+	return render(request, 'users/profile.html', context)
 
 
 
