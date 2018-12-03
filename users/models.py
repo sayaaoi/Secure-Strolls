8a057594317d670cf22395bd2cfe2e6d7ed0c128
Amber Wu <yuejun.wu1115@gmail.com>
2018-12-02 22:39:55 -0600

    resize image

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # One-to-one relationship with the existing user model
 class Profile(models.Model):
@@ -8,3 +9,16 @@ class Profile(models.Model):
 
 	def __str__(self):
 		return f'{self.user.username} Profile'
+
+
+	def save(self):
+		super().save()
+
+		# resize the upload image
+		img = Image.open(self.image.path)
+
+		# check if image is more than 300 pixel
+		if img.height > 300 or img.width > 300:
+			output_size = (300, 300)
+			img.thumbnail(output_size)
+			img.save()
