cb7b9d789470e71b0579498ce2b9d84293361e20
Amber Wu <yuejun.wu1115@gmail.com>
2018-12-02 21:46:02 -0600

    list of safest routes shown

--- welcome/views.py
+++ welcome/views.py
@@ -4,67 +4,86 @@ from django.db import connection
 import requests
 import json
 from django.template.response import TemplateResponse
+import logging
+
+logger = logging.getLogger(__name__)
+
+
 
 def welcome(request):
-	# function -> Michael
+    # function -> Michael
     return render(request, 'welcome/welcome.html')
 
+
 def enterloc(request):
-	req = requests.get("https://maps.googleapis.com/maps/api/directions/json?mode=walking&alternatives=true&origin=202+E+White+St+Champaign+IL&destination=1010+W+Springfield+Ave+Urbana+IL&key=AIzaSyCUV9mE32gI0uU2CpKcYjPXJbXliyTWrKo")
-	rjson = req.json()
-	routes = rjson["routes"]
-# 	for route in routes:
-	    
-	check = ''
-	for route in routes:
-		steps = route["legs"][0]["steps"]
-		for step in steps:
-			s_lat = step["start_location"]["lat"]
-			s_long = step["start_location"]["lng"]
-			e_lat = step["end_location"]["lat"]
-			e_long = step["end_location"]["lng"]
-			check = type(s_long)
-			if (s_long<e_long):
-				s_long -= 0.075
-				e_long += 0.075
-			else:
-				s_long += 0.075
-				e_long -= 0.075
-				e_long = s_long
-				s_long = e_long
-				e_long = temp
-	        
-			if (s_lat < e_lat):
-				s_lat -= 0.1125
-				e_lat += 0.1125
-			else:
-				s_lat += 0.1125
-				e_lat -= 0.1125
-				temp = s_lat
-				s_lat = e_lat
-				e_lat = temp
-	        
-			cursor = connection.cursor()
-			cursor.execute('SELECT SUM(Score) FROM INCIDENT JOIN Crime WHERE  INCIDENT.Crime_Type = Crime.Crime_Type JOIN LOCATION on INCIDENT.Location.ID = LOCATION.Location_ID WHERE Latitude > {0} and Latitude < {1} and Longitude > {2} and Longitude < {3};'.format(s_lat, e_lat, s_long, e_long))
-			rows = cursor.fetchall()
-			for row in rows:
-				check+= row + "\n"
-	        
+    req = requests.get(
+        "https://maps.googleapis.com/maps/api/directions/json?mode=walking&alternatives=true&origin=202+E+White+St+Champaign+IL&destination=1010+W+Springfield+Ave+Urbana+IL&key=AIzaSyCUV9mE32gI0uU2CpKcYjPXJbXliyTWrKo")
+    rjson = req.json()
+    routes = rjson["routes"]
+    # 	for route in routes:
 
-	arr = [check, 1]
-	return TemplateResponse(request, 'welcome/enterloc.html', {"test": arr})
+    checkarr = list()
+    check = -1
+    check2 = -1
+    i = 0
+    for route in routes:
+        steps = route["legs"][0]["steps"]
+        for step in steps:
+            s_lat = step["start_location"]["lat"]
+            s_long = step["start_location"]["lng"]
+            e_lat = step["end_location"]["lat"]
+            e_long = step["end_location"]["lng"]
+            check = type(s_long)
+            if (s_long < e_long):
+                s_long -= 0.075
+                e_long += 0.075
+            else:
+                s_long += 0.075
+                e_long -= 0.075
+                e_long = s_long
+                s_long = e_long
+                e_long = temp
 
-	# context = {
- #        'tests': tests
- #    }
-	# return render(request, 'welcome/enterloc.html', {'tests':'Work!'})
+            if (s_lat < e_lat):
+                s_lat -= 0.1125
+                e_lat += 0.1125
+            else:
+                s_lat += 0.1125
+                e_lat -= 0.1125
+                temp = s_lat
+                s_lat = e_lat
+                e_lat = temp
+
+            # cursor = connection.cursor()
+            # cursor.execute(
+            #     'SELECT Address, Latitude, Longitude, Score FROM INCIDENT JOIN Crime ON  INCIDENT.Crime_Type = Crime.Crime_Type JOIN LOCATION ON INCIDENT.Location_ID = LOCATION.Location_ID;')
+            # rows = cursor.fetchall()
+            # for row in rows:
+            #     checkarr.append(row[0])
+            
+            cursor = connection.cursor()
+            cursor.execute(
+                'SELECT Address FROM INCIDENT JOIN Crime ON  INCIDENT.Crime_Type = Crime.Crime_Type JOIN LOCATION ON INCIDENT.Location_ID = LOCATION.Location_ID WHERE Latitude > {0} and Latitude < {1} and Longitude > {2} and Longitude < {3};'.format(
+                    s_lat, e_lat, s_long, e_long))
+            rows = cursor.fetchall()
+            for row in rows:
+                checkarr.append(row)
+
+        i += 1
+    arr = [check, check2, checkarr]
+    return TemplateResponse(request, 'welcome/enterloc.html', {"test": arr})
+
+
+# context = {
+#        'tests': tests
+#    }
+# return render(request, 'welcome/enterloc.html', {'tests':'Work!'})
 # 	return render(request, 'welcome/enterloc.html', test)
-    
-	# return render(request, 'welcome/enterloc.html', {'che':checkstr})
+
+# return render(request, 'welcome/enterloc.html', {'che':checkstr})
 
 def result(request):
-	return render(request, 'welcome/result.html')
-
+    return render(request, 'welcome/result.html')
 
 # def myview(request):
 #   conn = MySQLdb.connect("connection info here")
