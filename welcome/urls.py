b4527cac25db8c452a133f11194f18775a591a13
Yuejun Wu <yuejun.wu1115@gmail.com>
2018-12-04 16:02:39 -0600

    Update urls.py

--- welcome/urls.py
+++ welcome/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('result/', views.result, name='welcome-result'),
     path('info/', views.result, name='info'),
     path('welcome/', views.enterloc, name='welcome-enterlocation'),
-    # path('result/', views.result, name='welcome-result'),
 ]
